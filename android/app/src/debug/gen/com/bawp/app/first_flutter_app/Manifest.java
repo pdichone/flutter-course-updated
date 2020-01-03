02281b8a1de156ffaeac7cbe4e2474f933bb74ca
Paulo <pdichone@gmail.com>
2020-01-02 18:48:35 -0800

    First Commit

---
+++ android/app/src/debug/gen/com/bawp/app/first_flutter_app/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.bawp.app.first_flutter_app;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
